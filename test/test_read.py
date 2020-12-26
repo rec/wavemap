7d59b467c018fbc61cbea6ecd4c32670d12e10c9
Tom Ritchford <tom@swirly.com>
2020-12-26 17:31:45 +0100

    New test/data with .wav files

---
+++ test/test_read.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+import stroll
+import unittest
+import wavemap
+
+WAVE_FILES = list(stroll(Path(__file__).parent, suffix='.wav'))
+READABLE = [
+    'Kick.wav',
+    'Snare.wav',
+    'test-44100Hz-2ch-32bit-float-le.wav',
+    'M1F1-float32-AFsp.wav',
+    'M1F1-float64-AFsp.wav',
+    'M1F1-int16-AFsp.wav',
+    'M1F1-int32-AFsp.wav',
+    'M1F1-uint8-AFsp.wav',
+    'stereofl.wav',
+    'stereol.wav',
+]
+
+
+class TestWaveMap(unittest.TestCase):
+    def test_existing(self):
+        success, failure = [], []
+        for w in WAVE_FILES:
+            try:
+                success.append(wavemap.WaveMap(w))
+            except Exception as e:
+                failure.append((w.stem, e.args[0]))
+
+        filenames = [s.filename.name for s in success]
+        assert filenames == READABLE
+        dtypes = [str(s.dtype) for s in success]
+        lengths = [s.shape[0] for s in success]
+        assert len(failure) == 25
+        tom = next(j for i, j in failure if i == 'Tom')
+        assert tom == 'Weird fmt block'
+
+        assert dtypes == [
+            'int16',
+            'float32',
+            'float32',
+            'float32',
+            'float64',
+            'int16',
+            'int32',
+            'uint8',
+            'float32',
+            'int16',
+        ]
+        assert lengths == [
+            10482,
+            3131,
+            440,
+            23493,
+            23493,
+            23493,
+            23493,
+            23493,
+            29015,
+            29015,
+        ]
+
+    def test_warnings(self):
+        warnings = []
+        for w in WAVE_FILES:
+            if w.name in READABLE:
+                warnings.append([])
+                wavemap.WaveMap(w, warn=warnings[-1].append)
+
+        expected = [
+            [],
+            [],
+            ['7 bytes after end-of-frame discarded'],
+            [],
+            [],
+            [],
+            [],
+            [],
+            ['7 bytes after end-of-frame discarded'],
+            ['3 bytes after end-of-frame discarded'],
+        ]
+
+        assert warnings == expected
+
+    def test_error(self):
+        with self.assertRaises(ValueError) as m:
+            wavemap.WaveMap(__file__)
+        assert m.exception.args[0] == 'Not a RIFF file'
--- test/test_write.py
+++ test/test_write.py
@@ -1,74 +1,13 @@
+from .test_read import WAVE_FILES, READABLE
 from numpy.testing import assert_array_equal
 from pathlib import Path
-import stroll
 import tdir
 import unittest
 import wavemap
 
-WAVE_FILES = list(stroll(Path(__file__).parent, suffix='.wav'))
-READABLE = [
-    'M1F1-float32-AFsp.wav',
-    'M1F1-float64-AFsp.wav',
-    'M1F1-int16-AFsp.wav',
-    'M1F1-int32-AFsp.wav',
-    'M1F1-uint8-AFsp.wav',
-    'stereofl.wav',
-    'stereol.wav',
-]
 
-
-class TestWaveMap(unittest.TestCase):
-    def test_existing(self):
-        success, failure = [], []
-        for w in WAVE_FILES:
-            try:
-                success.append(wavemap.WaveMap(w))
-            except Exception as e:
-                failure.append((w.stem, e.args[0]))
-
-        filenames = [s.filename.name for s in success]
-        assert filenames == READABLE
-        dtypes = [str(s.dtype) for s in success]
-        lengths = [s.shape[0] for s in success]
-
-        assert dtypes == [
-            'float32',
-            'float64',
-            'int16',
-            'int32',
-            'uint8',
-            'float32',
-            'int16',
-        ]
-        assert lengths == [23493, 23493, 23493, 23493, 23493, 29015, 29015]
-
-    def test_warnings(self):
-        warnings = []
-        for w in WAVE_FILES:
-            if w.name in READABLE:
-                warnings.append([])
-                wavemap.WaveMap(w, warn=warnings[-1].append)
-
-        expected = [
-            [],
-            [],
-            [],
-            [],
-            [],
-            ['7 bytes after end-of-frame discarded'],
-            ['3 bytes after end-of-frame discarded'],
-        ]
-
-        assert warnings == expected
-
-    def test_error(self):
-        with self.assertRaises(ValueError) as m:
-            wavemap.WaveMap(__file__)
-        assert m.exception.args[0] == 'Not a RIFF file'
-
-
-@tdir
 class TestWaveWrite(unittest.TestCase):
+    @tdir
     def test_write(self):
         filename = next(w for w in WAVE_FILES if 'int16' in w.stem)
         wm = wavemap.WaveMap(filename)
@@ -102,6 +41,7 @@ class TestWaveWrite(unittest.TestCase):
         assert s1 == 94174
         assert s2 == 94008
 
+    @tdir
     def test_int(self):
         for filename in WAVE_FILES:
             if not ('int' in filename.name and filename.name in READABLE):
@@ -137,5 +77,35 @@ class TestWaveWrite(unittest.TestCase):
 
             (s1,) = struct.unpack('<I', sb[4:8])
             (s2,) = struct.unpack('<I', b1[4:8])
-            assert 166 <= s1 - s2 <= 168
-            # What's going on here??
+            assert s1 - s2 == 166 == 0xA6
+
+    @tdir
+    def test_float(self):
+        for filename in WAVE_FILES:
+            if not ('float' in filename.name and filename.name in READABLE):
+                continue
+            wm = wavemap.WaveMap(filename)
+
+            tf1 = Path(filename.name)
+            wm1 = wavemap.new_like(tf1, wm)
+            assert_array_equal(wm, wm1)
+            wm1.flush()
+
+            tf2 = Path('2-' + filename.name)
+            wm2 = wavemap.new_like(tf2, wm)
+            assert_array_equal(wm, wm2)
+            wm2.flush()
+
+            b, b1, b2 = (
+                filename.read_bytes(),
+                tf1.read_bytes(),
+                tf2.read_bytes(),
+            )
+            assert len(b1) == len(b2), f'{len(b1)} == {len(b2)}'
+            assert b1 == b2
+            assert b
+
+            # sb = b[: len(b2)]
+            # differs = [i for i, (x, y) in enumerate(zip(sb, b2)) if x != y]
+            # # Should be zero!
+            # assert len(differs) == 1355, f'{len(sb)}'
