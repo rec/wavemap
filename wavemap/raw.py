a63ccf0e1b1a3b572c534a46aae7f49e8b573431
Tom Ritchford <tom@swirly.com>
2021-01-05 20:52:36 +0100

    Sketches toward reading 24-bit files

--- wavemap/raw.py
+++ wavemap/raw.py
@@ -1,5 +1,6 @@
 # from numpy.lib.stride_tricks import as_strided
 from .memmap import memmap
+from numpy.lib.stride_tricks import as_strided
 import numpy as np
 import sys
 
@@ -29,31 +30,29 @@ class RawMap(memmap):
         if offset < 0 or roffset < 0:
             raise ValueError('offset and roffset must be non-negative')
 
-        is_write = 'w' in mode
-        if is_write:
-            mode = 'w+'
-
-        if not shape:
-            if is_write:
-                raise ValueError('Must set a shape in write mode')
-            shape = (1,)
+        if order not in ('C', 'F', None):
+            raise ValueError(f'Bad order "{order}"')
 
         if isinstance(shape, int):
             shape = (shape,)
 
-        if not (1 <= len(shape) <= 2):
+        if shape and not (1 <= len(shape) <= 2):
             raise ValueError('Wave files must have 1 or 2 dimensions')
 
         is_int24 = str(dtype) == int24
         dt = np.dtype('uint8' if is_int24 else dtype)
 
+        is_write = 'w' in mode
         if is_write:
+            if not shape:
+                raise ValueError('Must set a shape in write mode')
             mode = 'w+'
             order = order or 'FC'[max(shape) == shape[0]]
-            channels = min(shape)
+            *rest, frame_count = sorted(shape)
+            channels = rest and rest[0] or 1
 
         else:
-            channels, *rest = sorted(shape)
+            channels, *rest = sorted(shape or (1,))
             frames_requested = rest and rest[0] or 0
 
             if is_int24:
@@ -74,7 +73,6 @@ class RawMap(memmap):
                     warn(f'{extra} byte{s} after end-of-frame discarded')
 
             order = order or 'C'
-
             if channels == 1 and not always_2d:
                 shape = (frame_count,)
             elif order == 'C':
@@ -93,8 +91,19 @@ class RawMap(memmap):
         self.roffset = roffset
 
         if is_int24 and allow_conversion:
-            length = max(self.shape)
-            length -= length % 12
+            # https://stackoverflow.com/a/34128171/4383
+
+            # length -= length % 12
+            # rawbytes = rawdatamap[:length]
+
+            # realdata = as_strided(
+            #     rawbytes.view(np.int32), strides=(12, 3,), shape=(frames, 4)
+            # )
+
+            # someusefulpart = (
+            #   realdata[hugeoffset : hugeoffset + smallerthanram] & 0x00FFFFFF
+            # )
+            assert as_strided
 
         return self
 
