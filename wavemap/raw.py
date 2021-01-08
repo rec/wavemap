1572678f366fcbc5546c41166adef5eb1a28d748
Tom Ritchford <tom@swirly.com>
2021-01-08 16:33:21 +0100

    Code finished for 24-bit, no tests

--- wavemap/raw.py
+++ wavemap/raw.py
@@ -27,6 +27,11 @@ class RawMap(memmap):
         allow_conversion=True,
         warn=warn,
     ):
+        def new(shape=shape, dtype=dtype, order=order, mode=mode):
+            return memmap.__new__(
+                cls, filename, dtype, mode, offset, shape, order, roffset
+            )
+
         if offset < 0 or roffset < 0:
             raise ValueError('offset and roffset must be non-negative')
 
@@ -43,75 +48,33 @@ class RawMap(memmap):
             if not shape:
                 raise ValueError('Must set a shape in write mode')
             order = order or 'FC'[max(shape) == shape[0]]
+            return new(mode='w+', order=order)
 
-            return memmap.__new__(
-                cls, filename, dtype, 'w+', offset, shape, order, roffset
-            )
-
-        is_int24 = str(dtype) == int24
-        if is_int24:
-            dt = np.dtype('uint8')
+        if str(dtype) == int24:
             itemsize = 3
-            frame_scale = 3
         else:
-            dt = np.dtype(dtype)
-            itemsize = dt.itemsize
-            frame_scale = 1
-
-        channels, *rest = sorted(shape or (1,))
-        frames_requested = rest and rest[0] or 0
+            itemsize = np.dtype(dtype).itemsize
 
         file_size = file_byte_size(filename)
         audio_size = file_size - offset - roffset
-        if is_int24:
-            extra = audio_size % 12
-            if extra and warn:
-                s = 's' if len(extra) != 1 else ''
-                warn(f'24-bit conversion lost last {extra} byte{s}, sorry')
-            audio_size -= extra
+        shape = _get_shape(shape, audio_size, itemsize, order, always_2d, warn)
+        if itemsize != 3:
+            return new(shape=shape)
 
-        frame_size = itemsize * channels
-        frames = audio_size // frame_size
+        frames = shape[0] * (shape[1] if len(shape) else 1)
+        assert not (frames % 4)
 
-        if frames_requested and frames_requested < frames:
-            warn(f'Requested {frames_requested} frames, got {frames}')
-            frames = frames_requested
+        # https://stackoverflow.com/a/34128171/4383
+        raw = new(shape=(itemsize * frames // 4,), dtype='int32')
+        strided = as_strided(raw, strides=(12, 3,), shape=(frames, 4))
+        reshaped = np.reshape(strided, shape=shape)
 
-        if warn:
-            extra = audio_size % frame_size
-            if extra:
-                s = '' if extra == 1 else 's'
-                warn(f'{extra} byte{s} after end-of-frame discarded')
+        if not allow_conversion:
+            return reshaped
 
-        order = order or 'C'
-
-        if channels == 1 and not always_2d:
-            shape = (frames * frame_scale,)
-        elif order == 'C':
-            shape = frames, channels * frame_scale
-        else:
-            shape = channels * frame_scale, frames
-
-        self = memmap.__new__(
-            cls, filename, dt, mode, offset, shape, order, roffset
-        )
-
-        if is_int24 and allow_conversion:
-            # https://stackoverflow.com/a/34128171/4383
-
-            # length -= length % 12
-            # rawbytes = rawdatamap[:length]
-
-            # realdata = as_strided(
-            #     rawbytes.view(np.int32), strides=(12, 3,), shape=(frames, 4)
-            # )
-
-            # someusefulpart = (
-            #   realdata[hugeoffset : hugeoffset + smallerthanram] & 0x00FFFFFF
-            # )
-            assert as_strided
-
-        return self
+        result = reshaped & 0x00FFFFFF
+        result *= 0x100
+        return result
 
 
 def file_byte_size(filename):
@@ -125,3 +88,36 @@ def dump_locals():
     for k, v in locals().items():
         if isinstance(v, (Number, tuple, str)):
             print(f'{k} = {v!r}')
+
+
+def _get_shape(shape, audio_size, itemsize, order, always_2d, warn):
+    channels, *rest = sorted(shape or (1,))
+    frames_requested = rest and rest[0] or 0
+
+    if itemsize == 3:
+        extra = audio_size % 12
+        if extra and warn:
+            s = 's' if len(extra) != 1 else ''
+            warn(f'24-bit conversion lost last {extra} byte{s}, sorry')
+        audio_size -= extra
+
+    frame_size = itemsize * channels
+    frames = audio_size // frame_size
+
+    if frames_requested and frames_requested < frames:
+        warn(f'Requested {frames_requested} frames, got {frames}')
+        frames = frames_requested
+
+    if warn:
+        extra = audio_size % frame_size
+        if extra:
+            s = '' if extra == 1 else 's'
+            warn(f'{extra} byte{s} after end-of-frame discarded')
+
+    if channels == 1 and not always_2d:
+        return (frames,)
+
+    if not order or order == 'C':
+        return frames, channels
+
+    return channels, frames
